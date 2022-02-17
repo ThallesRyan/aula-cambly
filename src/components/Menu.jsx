57c70322f33f9091afc4fd65bc2622c8b58b6a70
ThallesRyan <thallesryan2@gmail.com>
2022-02-17 17:00:15 -0300

    Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,4 +1,10 @@
 import "./menu.css"
+import { BsChatLeftTextFill } from "react-icons/bs"
+import { BsCalendarEvent } from "react-icons/bs"
+import { FiBell } from 'react-icons/fi'
+import { AiFillQuestionCircle } from 'react-icons/ai'
+import { AiFillGoogleCircle } from 'react-icons/ai'
+
 export default (props) => {
     return (
         <div className="menu">
@@ -16,21 +22,24 @@ export default (props) => {
                 <div>
                     <button className="botaoPlano"> ESCOLHA SEU PLANO</button>
                 </div>
+
                 <div>
-                    (Conversas)
-                </div>
-                <div>
-                    (sininho)
-                </div>
-                <div>
-                    Calendário
+                    <a className="iconsLink" href=""><BsChatLeftTextFill className="icons" /></a>
                 </div>
 
                 <div>
-                    ?
+                    <FiBell />
                 </div>
 
-                <div>Conta Google</div>
+                <div>
+                    <BsCalendarEvent />
+                </div>
+
+                <div>
+                   <AiFillQuestionCircle />
+                </div>
+
+                <div><AiFillGoogleCircle /></div>
             </div>
         </div>
     )
